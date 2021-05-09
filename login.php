bd912b66bdb2fe68fc552e046f867b211d716640
Dansleym <Dansleymeplz@gmail.com>
2021-05-09 12:14:02 +0300

    Dynamic users, add db tables users and messange,

--- login.php
+++ login.php
@@ -1,27 +1,17 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Chat</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-</head>
+<?php include_once "header.php";?>
 <body>
     <div class="wrapper">
-        <section class="form signup">
+        <section class="form login">
             <header>Chat</header>
-            <form action="#">
-                <div class="error-txt">This is an error massage!</div>
+            <form action="#" autocomplete="off">
+                <div class="error-txt"></div>
                 <div class="field input">
                     <label>Email Address</label>
-                    <input type="text" placeholder="Enter your email">
+                    <input type="text" name="email" placeholder="Enter your email">
                 </div>
                 <div class="field input">
                     <label>Password</label>
-                    <input type="password" placeholder="Enter new password">
+                    <input type="password" name="password" placeholder="Enter new password">
                     <i class="fa fa-eye"> </i>
                 </div>
                 <div class="field button">
@@ -33,7 +23,7 @@
     </div>
 
     <script src="js/pswd-toggle.js"></script>
-    <script src="js/signup.js"></script>
+    <script src="js/login.js"></script>
 </body>
 </html>
 
